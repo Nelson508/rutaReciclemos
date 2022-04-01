f3836c5ddc808fe9d4257d2f0f8d326597157f8a
LuisMoyanoIT <luismocru@gmail.com>
2022-04-01 07:10:54 -0600

    Navegación + componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { InformacionComponent } from './pages/informacion/informacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NosotrosComponent,
+    InicioComponent,
+    InformacionComponent
   ],
   imports: [
     BrowserModule,
