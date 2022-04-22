3fc8148ad183ccd2848c0bf22385c0d4623601a2
Nelson Dominguez <ndominguez.riveras@gmail.com>
2022-04-22 15:35:44 -0400

    trazabilidad errores

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -14,8 +14,6 @@ export class InicioComponent implements OnInit {
   mostrarTexto4 = false;
   mostrarTexto5 = false;
 
-  hover: boolean = false;
-
   constructor() { }
 
   ngOnInit(): void {}
@@ -30,11 +28,11 @@ export class InicioComponent implements OnInit {
 
 
   hoverStateIn(aux: any){
-    this.mostrarTexto1 = (aux==1) ? true: false;
-    this.mostrarTexto2 = (aux==2) ? true: false;
-    this.mostrarTexto3 = (aux==3) ? true: false;
-    this.mostrarTexto4 = (aux==4) ? true: false;
-    this.mostrarTexto5 = (aux==5) ? true: false;
+    this.mostrarTexto1 = (aux==1) ? true : false;
+    this.mostrarTexto2 = (aux==2) ? true : false;
+    this.mostrarTexto3 = (aux==3) ? true : false;
+    this.mostrarTexto4 = (aux==4) ? true : false;
+    this.mostrarTexto5 = (aux==5) ? true : false;
   }
 
   hoverStateOut(){
@@ -45,5 +43,28 @@ export class InicioComponent implements OnInit {
     this.mostrarTexto5 = false;
   }
 
+  public touchDown(aux: number) {
+    this.mostrarTexto1 = (aux==1) ? !this.mostrarTexto1 : false;
+    this.mostrarTexto2 = (aux==2) ? !this.mostrarTexto2 : false;
+    this.mostrarTexto3 = (aux==3) ? !this.mostrarTexto3 : false;
+    this.mostrarTexto4 = (aux==4) ? !this.mostrarTexto4 : false;
+    this.mostrarTexto5 = (aux==5) ? !this.mostrarTexto5 : false;
+  }
+
+  /* public touchLeave() {
+    this.mostrarTexto1 = false;
+    this.mostrarTexto2 = false;
+    this.mostrarTexto3 = false;
+    this.mostrarTexto4 = false;
+    this.mostrarTexto5 = false;
+  }
+  
+  public touchUp() {
+    this.mostrarTexto1 = false;
+    this.mostrarTexto2 = false;
+    this.mostrarTexto3 = false;
+    this.mostrarTexto4 = false;
+    this.mostrarTexto5 = false;
+  } */
 }
 
